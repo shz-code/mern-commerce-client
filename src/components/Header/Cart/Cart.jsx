415140d94c60ad89ba729d1372558621d0020b4a
shz-code <web.shahidul.alam@gmail.com>
2024-02-10 15:08:48 +0600

    modify: headerCart

--- src/components/Header/Cart/Cart.jsx
+++ src/components/Header/Cart/Cart.jsx
@@ -1,4 +1,3 @@
-import { User2Icon } from "lucide-react";
 import { Link } from "react-router-dom";
 import Button from "../../ui/Button";
 
@@ -18,15 +17,27 @@ const Cart = ({ setCartOpen, cartOpen }) => {
           <div className="mt-2">
             <p className="text-4xl font-extrabold text-white">Cart</p>
             <div className="cartItems mt-4">
-              <div className="bg-slate-100 rounded px-2 py-4 flex gap-4 items-center">
-                <span>
-                  <User2Icon />
-                </span>
-                <div className="w-full">
-                  <p>Name</p>
-                  <div className="flex justify-between">
-                    <span>Quantity</span>
-                    <span>x1</span>
+              <div className="bg-slate-100 rounded grid gap-4 items-center">
+                <div className="w-full p-4 border-b">
+                  <p className="text-xl font-semibold tracking-tighter">Name</p>
+                  <div className="flex justify-between items-center">
+                    <span className="text-xs">Quantity</span>
+                    <span className="text-lg font-semibold">x1</span>
+                  </div>
+                </div>
+                <div className="px-4 pb-4 flex justify-between items-center">
+                  <Link to={`/product/`} className="w-16">
+                    <img src="https://erabanbd.com/wp-content/uploads/2024/01/qcy-watch-gt.png" />
+                  </Link>
+                  <div className="gap-2 text-xs w-1/2">
+                    <p className="border-b pb-2 text-end">
+                      Unit Price:{" "}
+                      <span className="text-sm font-bold">200৳</span>
+                    </p>
+                    <p className="w-full pt-2 text-end">
+                      Total Price:{" "}
+                      <span className="text-sm font-bold">200৳</span>
+                    </p>
                   </div>
                 </div>
               </div>
